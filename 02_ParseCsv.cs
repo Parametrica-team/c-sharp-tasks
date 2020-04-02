df596c9d5666cd653611347273bba5281ec371bb
Max <mmm@parametrica.team>
2020-04-02 22:56:49 +0300

    второе задание

---
+++ 02_ParseCsv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+	public Program()
+	{
+		static void Main(string[] args)
+        {
+			var path = @"E:\Repos\ROBOT_BI\Modules\Facades\Facade Blocks.csv";
+			var key = "kit_in_sec_0";
+			
+			string facadeNames = GetData(path, key);
+
+			string facadeName = GetRandom(facadeNames);
+        }
+
+
+	}
+}
