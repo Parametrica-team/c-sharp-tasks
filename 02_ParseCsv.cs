bb41fea01715135a817a5b1fb80214ddc27a0fc2
ruslanrs <srr@parametrica.team>
2020-04-03 12:30:32 +0300

    добавлен метод GetData

--- 02_ParseCsv.cs
+++ 02_ParseCsv.cs
@@ -5,7 +5,22 @@ namespace Task2
 {
 	public class Program
 	{
-		static void Main(string[] args)
+        private static string GetData(string path, string key)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+
+                var parts = lines[i].Split(';');
+                var a = parts[0];
+                if (a == key)
+                    return parts[1];
+            }
+            return null;
+        }
+
+        static void Main(string[] args)
         {
 			var path = @"E:\Repos\ROBOT_BI\Modules\Facades\Facade Blocks.csv";
 			var key = "kit_in_sec_0";
