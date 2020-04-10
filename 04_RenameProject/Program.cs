b6e1f0da42dd2b675b3614d09954850fe05f852b
Max <mmm@parametrica.team>
2020-04-10 22:10:04 +0300

    Решение задачи 04

--- 04_RenameProject/Program.cs
+++ 04_RenameProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace _04_RenameProject
 {
@@ -31,7 +32,7 @@ namespace _04_RenameProject
             var openFileDialog = new System.Windows.Forms.OpenFileDialog()
             {
                 Title = "Выберите файл c таблицей переименований",
-                Filter = "txt files (*.txt)|*.txt|csv files (*.csv)|*.csv",
+                Filter = "csv files (*.csv)|*.csv|txt files (*.txt)|*.txt",
                 RestoreDirectory = true
             };
             result = openFileDialog.ShowDialog(); // открывает окно выбора файла
@@ -46,39 +47,91 @@ namespace _04_RenameProject
 
 
             //Найти все файлы JSON
-            string[] jsonPaths = GetFileNames(folderPath, ".json");
+            var jsonPaths = GetFileNames(folderPath, ".json");
             if (jsonPaths == null)
             {
                 Console.WriteLine("нет файлов .json");
                 Console.ReadKey();
                 return;
             }
-                
-
-
 
             //Переименовать
             foreach (var jsonPath in jsonPaths)
             {
-                bool success = RenameProperties(jsonPath, filePath);
-                
-                //Вывести на коснсоль результат в виде: С:/.../вася.json переименован (или "не найдено подходящих свойств")
+                string json = RenameProperties(jsonPath, filePath);
+                if (!string.IsNullOrEmpty(json))
+                {
+                    System.IO.File.WriteAllText(jsonPath, json);
+                    Console.WriteLine($"Файл {jsonPath} перезаписан");
+                }
+                else
+                {
+                    Console.WriteLine($"В файле {jsonPath} не найдено подходящих свойств");
+                }
             }
+            Console.WriteLine();
+            Console.WriteLine("Перезапись завершена, нажмите любую клавишу");
+            Console.ReadKey();
         }
 
-        private static bool RenameProperties(string jsonPath, string filePath)
+        private static string RenameProperties(string jsonPath, string csvPath)
         {
-            
-            throw new NotImplementedException();
-            return true;
+            if (string.IsNullOrEmpty(jsonPath) || string.IsNullOrEmpty(jsonPath)) 
+                return null;
+
+            string json;
+            string[] lines;
+            try
+            {
+                lines = System.IO.File.ReadAllLines(csvPath, Encoding.Default);
+                json = System.IO.File.ReadAllText(jsonPath, Encoding.Default);
+            }
+            catch
+            {
+                return null;
+            }
+
+            bool result = false;
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(';');
+                if (parts.Length < 2)
+                    continue;
+
+                var key = parts[0].Trim();
+                var value = parts[1].Trim();
+
+                string pattern = @"(?<= """ + key + @""":\s"")[\w\s]+(?="")";
+
+                if (Regex.IsMatch(json, pattern))
+                {
+                    json = Regex.Replace(json, pattern, value);
+                    result = true;
+                }
+            }
+
+            if (result)
+                return json;
+            else
+                return null;
         }
 
-        private static string[] GetFileNames(string folderPath, string extension)
+        private static List<string> GetFileNames(string folderPath, string extension)
         {
             var allFiles = System.IO.Directory.GetFiles(folderPath, "*", System.IO.SearchOption.AllDirectories);
-            // доделать
+            if (allFiles == null)
+                return null;
 
-            return null;
+            var jsons = new List<string>();
+
+            foreach (var path in allFiles)
+            {
+                if (System.IO.Path.GetExtension(path).ToLower() == extension.ToLower())
+                    jsons.Add(path);
+            }
+
+            return jsons;
         }
     }
 }
