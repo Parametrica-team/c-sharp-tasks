6ed5a8929b6ff63283e281a1d1e5dfcbfcc1e2db
ruslanrs <srr@parametrica.team>
2020-05-26 00:56:48 +0300

    сделал расчет угла в окне

---
+++ 05_WindowRecess/WindowRecess/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowRecess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double GetAngle(double height, double depth)
+            {
+                var angle = Math.PI/2-(Math.Atan((depth / (height / 2)) + Math.PI*0));
+                return angle;
+            }
+
+            Console.WriteLine("Введите высоту окна:");
+            string hieghtA = Console.ReadLine();
+            Console.WriteLine("Введите глубину окна:");
+            string depthB = Console.ReadLine();
+            var resultRadians = GetAngle(double.Parse(hieghtA), double.Parse(depthB));
+            var resultAngle = resultRadians*(180/ Math.PI);
+            Console.WriteLine("Угол в градусах");
+            Console.WriteLine(resultAngle);
+            Console.WriteLine("Угол в радианах");
+            Console.WriteLine(resultRadians);
+            Console.ReadKey();
+        }
+    }
+}
