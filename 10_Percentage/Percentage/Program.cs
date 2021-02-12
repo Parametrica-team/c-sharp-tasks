166a17ed10423160d6d611ddc85043f35b6d6012
Max <max@malein.com>
2021-02-12 22:52:56 +0300

    метод и тесты

--- 10_Percentage/Percentage/Program.cs
+++ 10_Percentage/Percentage/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Percentage
 {
@@ -10,11 +11,51 @@ namespace Percentage
             Console.WriteLine("Hello World!");
         }
 
-        public static List<int> AdjustPercenage(List<double> numbers)
+        public static List<int> AdjustPercentage(List<double> numbers)
         {
-            var result = new List<int>();
+            if (numbers == null)
+                return null;
 
-            // action
+            if (numbers.Sum() != (int)numbers.Sum())
+            {
+                throw new ArgumentException("Сумма аргументов должна быть целым числом");
+            }
+
+            var result = numbers.Select(n => (int)Math.Round(n)).ToList();
+
+            // если разница меньше нуля, то значит нужно уменьшить числа
+            // если разница больше нуля, то увеличить
+            // теоритически разница может быть больше 1 и нужно будет поправить несколько чисел!
+            int diff = (int)(numbers.Sum() - result.Sum());
+
+            if (diff < 0)
+            {
+                // нужно отсортировать индексы исходных значений в зависимости от дробной части
+                var sortedIndexes = numbers.Select((num, index) => new { num, index })
+                    .Where(pair => pair.num % 1 >= 0.5)
+                    .OrderBy(pair => pair.num % 1)
+                    .Select(pair => pair.index)
+                    .ToList();
+
+                var indexes = sortedIndexes.Take(-diff).ToList();
+
+                // уменьшить значения по получившимся индексам
+                indexes.ForEach(i => result[i]--);
+            }
+            else if (diff > 0)
+            {
+                // нужно отсортировать индексы исходных значений в зависимости от дробной части
+                var sortedIndexes = numbers.Select((num, index) => new { num, index })
+                    .Where(pair => pair.num % 1 > 0 && (pair.num % 1) < 0.5)
+                    .OrderByDescending(pair => pair.num % 1)
+                    .Select(pair => pair.index)
+                    .ToList();
+
+                var indexes = sortedIndexes.Take(diff).ToList();
+
+                // увеличить значения по получившимся индексам
+                indexes.ForEach(i => result[i]++);
+            }
 
             return result;
         }
--- 10_Percentage/PercentageTests/UnitTest1.cs
+++ 10_Percentage/PercentageTests/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace PercentageTests
         public void AdjustPercenage_Test_99()
         {
             var input = new List<double> {80.4, 9.3, 10.3 };
-            var result = Percentage.Program.AdjustPercenage(input);
+            var result = Percentage.Program.AdjustPercentage(input);
             Assert.AreEqual(100, result.Sum());
         }
 
@@ -23,8 +23,18 @@ namespace PercentageTests
         public void AdjustPercenage_Test_RoundMax()
         {
             var input = new List<double> { 9.3, 80.4, 10.3 };
-            var result = Percentage.Program.AdjustPercenage(input);
+            var result = Percentage.Program.AdjustPercentage(input);
             Assert.AreEqual(81, result[1], "80.4 должно было округлиться до 81");
+        }
+
+        
+
+        [Test]
+        public void AdjustPercenage_Test_RoundMax2()
+        {
+            var input = new List<double> { 80.30, 10.90, 9.30, 5.80, 14.40, 1.30, 1.00 };
+            var result = Percentage.Program.AdjustPercentage(input);
+            Assert.AreEqual(15, result[4], "14.4 должно было округлиться до 15");
         }
     }
 }
