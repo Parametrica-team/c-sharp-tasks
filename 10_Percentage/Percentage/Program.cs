08a5939d12daa389757512f960123a4d6258bbba
Max <max@malein.com>
2021-02-12 21:04:46 +0300

    Проект для подсчета процентов

---
+++ 10_Percentage/Percentage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Percentage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static List<int> AdjustPercenage(List<double> numbers)
+        {
+            var result = new List<int>();
+
+            // action
+
+            return result;
+        }
+    }
+}
