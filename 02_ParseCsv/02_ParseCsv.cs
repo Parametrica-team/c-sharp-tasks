7bb8a32e3e65533f0591b8cb391c211011c13be4
Max <max@malein.com>
2021-02-13 20:54:54 +0300

    вернул к корням

--- 02_ParseCsv/02_ParseCsv.cs
+++ 02_ParseCsv/02_ParseCsv.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 
 namespace Task2
 {
-	public class Program
-	{
+    public class Program
+    {
         static void Main(string[] args)
         {
-            var path = @"C:\Users\Pm\Desktop\ROBOT_BI\Modules\Facades\Facade Blocks.csv";
+            var path = @"E:\Repos\ROBOT_BI\Modules\Facades\Facade Blocks.csv";
             var key = "kit_in_sec_1";
 
             string facadeNames = GetData(path, key);
@@ -17,41 +18,13 @@ namespace Task2
             Console.WriteLine(facadeName);
             Console.ReadLine();
         }
-        private static string GetData(string path, string key)
-        {
-            {
-                string[] lines = System.IO.File.ReadAllLines(path);
-                var lngth = key.Length;
-
-                foreach (string line in lines)
-                {
-                    var a = line.Split(';');
-                    var x = a[0];
-
-                    if (string.IsNullOrEmpty(x))
-                        Console.WriteLine("false");
-
-                    else
-
-                    {
-                        var input = line[0];
-                        var result = line[1];
-                    }
-                }
-                
-            }
-            return [1];
-        }
 
         private static string GetRandom(string text)
         {
-            Random rnd = new Random();
-            var str = text.Split(',');
-            int i = rnd.Next(str.Length);
-
-            return str[i].TrimStart();
-
         }
 
+        private static string GetData(string path, string key)
+        {
+        }
     }
 }
